846b003178c00a3bd02a7a0f9e20002c59238174
jonathan serafini <jonathan.serafini@gmail.com>
2023-03-02 23:47:21 +0100

    init

---
+++ app/app.py
@@ -0,0 +1,38 @@
+import streamlit as st
+
+import numpy as np
+import pandas as pd
+
+st.markdown("""# This is a header
+## This is a sub header
+This is text""")
+
+df = pd.DataFrame({
+    'first column': list(range(1, 11)),
+    'second column': np.arange(10, 101, 10)
+})
+
+# this slider allows the user to select a number of lines
+# to display in the dataframe
+# the selected value is returned by st.slider
+line_count = st.slider('Select a line count', 1, 10, 3)
+
+# and used to select the displayed lines
+head_df = df.head(line_count)
+
+head_df
+
+@st.cache
+def get_histo():
+
+    df = pd.DataFrame(
+            np.random.randn(200, 1),
+            columns=['a']
+        )
+
+    return np.histogram(
+        df.a, bins=25)[0]
+
+hist_values = get_histo()
+
+st.bar_chart(hist_values)
